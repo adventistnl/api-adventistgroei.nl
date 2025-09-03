6ef6b977bb6ed935c4c97614e7effb6e1b6ca186
Maykel Felipe Nekel <maykelnekel@gmail.com>
2025-09-03 00:24:59 -0300

    fix: port name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.API_PORT || 3000;
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(Number(PORT));
   console.info(`Application is running on: http://localhost:${PORT}`);
