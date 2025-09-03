b62d3ab469e81de17bb22d84c04bf9ee2cda8a5d
Maykel Felipe Nekel <maykelnekel@gmail.com>
2025-09-03 00:22:26 -0300

    fix: app port handle

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.API_PORT;
-
 async function bootstrap() {
+  const PORT = process.env.API_PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(Number(PORT ?? 3000));
-  console.info(`Application is running on: http://localhost:${PORT ?? 3000}`);
+  await app.listen(Number(PORT));
+  console.info(`Application is running on: http://localhost:${PORT}`);
+  console.log('Memória usada:', process.memoryUsage());
 }
 
 void bootstrap();
