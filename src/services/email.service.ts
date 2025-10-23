d0d451f7a4a595845e2c75262e1ccc25f7728fd6
Maykel Felipe Nekel <maykelnekel@gmail.com>
2025-10-23 07:51:16 -0300

    fix: email imports

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { InviteEmailDto } from 'src/dto/email.dto';
-import { SendgridEmailRepository } from '../repositories/emails/sendgrid.repository';
-import { NodemailerEmailRepository } from 'src/repositories/emails/nodemailer.repository';
+import { InviteEmailDto } from '../dto/email.dto';
+import { NodemailerEmailRepository } from '../repositories/emails/nodemailer.repository';
 
 @Injectable()
 export class EmailService {
   constructor(
-    private readonly sendgridEmailRepository: SendgridEmailRepository,
     private readonly nodemailerRepository: NodemailerEmailRepository
   ) {}
 
   async sendInviteEmail(data: InviteEmailDto): Promise<void> {
     // await this.sendgridEmailRepository.sendInviteEmail(data);
     await this.nodemailerRepository.sendInviteEmail(data);
-  }
+  } 
 }
